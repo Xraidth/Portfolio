49db8d5d2c011e059ddda89f45adbedb3c64ab27
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-01-31 11:41:29 -0300

    Myblog 20240131

---
+++ portfolio/src/complements/MyBlog/visual/ListArticles.js
@@ -0,0 +1,27 @@
+
+import {React} from 'react'
+import Article from "../visual/Article.js";
+
+const ListArticles = ({data_art,blogs_filter}) => {
+
+  
+  let blog_data =  blogs_filter ? data_art.filter((x)=>x.title.toLowerCase().includes(`${blogs_filter.toLowerCase()}`)) : data_art;
+
+  if(blog_data){
+    blog_data = blog_data.sort((a, b) => new Date(b.art_date) - new Date(a.art_date));
+  }
+ 
+  const articles_arm = blog_data.map(function(e, index){
+        return(<Article key={index} title={e.title} introduction={e.introduction} body={e.body} conclusion = {e.conclusion} art_date={e.art_date}/>)
+        });
+
+
+  return (
+    <div className="custom-article-pr-bg d-flex flex-column align-items-center justify-content-between">
+      
+      {articles_arm}
+    </div>
+  )
+}
+
+export default ListArticles
--- portfolio/src/complements/MyBlog/visual/MyBlogNav.js
+++ portfolio/src/complements/MyBlog/visual/MyBlogNav.js
@@ -1,13 +1,28 @@
-import React from 'react'
 
-const MyBlogNav = () => {
+import {React, useState} from 'react'
+
+const MyBlogNav = ({handleFilter}) => {
+  const [txtSearch, setTxtSearch] = useState("");
+
+
+ function handleClick(e){
+  e.preventDefault();
+  handleFilter(txtSearch);
+ }
+
+
+  function handletxtSearch(e){
+    setTxtSearch(e.target.value);
+  }
+
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
       <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={handletxtSearch}/>
+        <button className="btn btn-outline-success" type="submit" onClick={handleClick}>Search</button>
       </form>
     </div>
 </nav>
