b74793b86cef973e6d85dcceb464da001efb4522
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-02-02 11:56:31 -0300

    custom copied 20240202

--- portfolio/src/complements/portfolio/visual/CustomBottomBox.js
+++ portfolio/src/complements/portfolio/visual/CustomBottomBox.js
@@ -1,29 +1,41 @@
-import React from 'react'
+
+import {React, useState} from 'react'
 
 const CustomBottomBox = ({textToCopy}) => {
+  const [clipBo, setCliBo] = useState(false);
 
   const handleCopyClick = async () => {
     try {
       await navigator.clipboard.writeText(textToCopy);
+      setCliBo(true);
+
+      setTimeout(() => {
+        setCliBo(false);
+      }, 3000);
+
+
     } catch (error) {
       console.error('Error when writting clipboard:', error);
+      setCliBo(false);
     }
   };
 
 
+
   return (
       <div>
-        <button className="btn">
+        <button className="btn" onClick={handleCopyClick} >
           <span>
-              <img src='../../../../../ButtonCopiar.png' className="img-fluid mini-icono" alt="Copy" title="Copy" onClick={handleCopyClick}></img>
+              <img src='../../../../../ButtonCopiar.png' className="img-fluid mini-icono" alt="Copy" title="Copy" ></img>
+              <p className={`text-light position-absolute bottom-50 start-0 ${clipBo ? "d-block":"d-none"}`} >Copied</p>
           </span>
         </button>
 
-        <button className="btn">
+        <a className="btn" href={`mailto:${textToCopy}`}>
           <span>
               <img src='../../../../../ButtonEnviar.png' className="img-fluid mini-icono" alt="Send Email" title="Send Email"></img>
           </span>
-        </button>
+        </a>
       </div>
      
   )
